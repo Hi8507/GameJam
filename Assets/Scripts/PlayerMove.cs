17b8a53989e85393d636e5be23c9fdd5f8214121
Hi8507 <omerhalitipekci1006@gmail.com>
2025-04-19 22:37:50 +0300

    Moving on

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,21 +11,111 @@ public class PlayerMove : MonoBehaviour
 
     private RaycastHit[] Hits = new RaycastHit[1];
 
+    public bool playerCanMove = true;
+    public float walkSpeed = 5f;
+    public float maxVelocityChange = 10f;
+    private bool isWalking = false;
+
+    public bool enableJump = true;
+    public KeyCode jumpKey = KeyCode.Space;
+    public float jumpPower = 5f;
+    private bool isGrounded = false;
+    public bool Jumped = false;
+
+    private Rigidbody rb;
+
     private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
         Agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Mouse0))
+        if (Input.GetKeyUp(KeyCode.W))
         {
-            Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+            
+            //Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.RaycastNonAlloc(ray, Hits) > 0)
+            //if (Physics.RaycastNonAlloc(ray, Hits) > 0)
+            //{
+            //    Agent.SetDestination(Hits[0].point);
+            //}
+        }
+
+        if (enableJump && Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            Jump();
+        }
+        CheckGround();
+    }
+    private void FixedUpdate()
+    {
+        if (playerCanMove)
+        {
+
+            Vector3 targetVelocity = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
+
+            if (targetVelocity.x != 0 || targetVelocity.z != 0 && isGrounded)
             {
-                Agent.SetDestination(Hits[0].point);
+                isWalking = true;
             }
+            else
+            {
+                isWalking = false;
+            }
+
+            targetVelocity = transform.TransformDirection(targetVelocity) * walkSpeed;
+
+            // Apply a force that attempts to reach our target velocity
+            Vector3 velocity = rb.velocity;
+            Vector3 velocityChange = (targetVelocity - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+
+            rb.AddForce(velocityChange, ForceMode.VelocityChange);
+
+
+        }
+
+
+
+        // All movement calculations while walking
+
+        
+    }
+
+    
+    private void CheckGround()
+    {
+        Vector3 origin = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * .5f), transform.position.z);
+        Vector3 direction = transform.TransformDirection(Vector3.down);
+        float distance = .75f;
+
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, distance))
+        {
+            Debug.DrawRay(origin, direction * distance, Color.red);
+            isGrounded = true;
+
+        }
+        else
+        {
+            isGrounded = false;
         }
     }
+
+    private void Jump()
+    {
+        // Adds force to the player rigidbody to jump
+        if (isGrounded)
+        {
+            Jumped = true;
+            rb.AddForce(0f, jumpPower, 0f, ForceMode.Impulse);
+            isGrounded = false;
+
+        }
+
+    }
+
 }
