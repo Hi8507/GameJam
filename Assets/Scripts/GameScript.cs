c3d14626a579289d7fc0d12f0e7865a1f1b94f17
Hi8507 <omerhalitipekci1006@gmail.com>
2025-04-20 15:30:16 +0300

    kdshkfb

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -21,10 +21,10 @@ public class GameScript : MonoBehaviour
 
     public int seconds;
 
-    public GameObject Yellowpill, Pinkpill;
+    public GameObject Yellowpill, Pinkpill,purplepill;
 
 
-
+    public GameObject hand1, hand2, hand3, hand4, hand5, hand6, hand11, hand12, hand13, hand14, hand15, hand16, hand17, hand18, hand19, hand110, hand111;
 
 
     // Start is called before the first frame update
@@ -44,7 +44,8 @@ public class GameScript : MonoBehaviour
                     // StartTime = 0;
                     StartTime = 0;
                     RemainingTime += Time.deltaTime;
-                    StartTime += startNumber+ Yellowpill.GetComponent<TriggerPills>().SanityPoints;
+                StartTime += startNumber + Yellowpill.GetComponent<TriggerPills>().SanityPoints + Pinkpill.GetComponent<TriggerPills>().SanityPoints + purplepill.GetComponent<TriggerPills>().SanityPoints
+                + hand1.GetComponent<handtrigger>().SanityPoints+ hand2.GetComponent<handtrigger>().SanityPoints + hand3.GetComponent<handtrigger>().SanityPoints + hand4.GetComponent<handtrigger>().SanityPoints + hand5.GetComponent<handtrigger>().SanityPoints + hand6.GetComponent<handtrigger>().SanityPoints + hand11.GetComponent<handtrigger>().SanityPoints + hand12.GetComponent<handtrigger>().SanityPoints + hand13.GetComponent<handtrigger>().SanityPoints + hand14.GetComponent<handtrigger>().SanityPoints + hand15.GetComponent<handtrigger>().SanityPoints + hand16.GetComponent<handtrigger>().SanityPoints + hand17.GetComponent<handtrigger>().SanityPoints + hand18.GetComponent<handtrigger>().SanityPoints + hand19.GetComponent<handtrigger>().SanityPoints + hand110.GetComponent<handtrigger>().SanityPoints + hand111.GetComponent<handtrigger>().SanityPoints;
                     seconds = Mathf.FloorToInt(RemainingTime % 60);
 
                     StartTime -= seconds;
